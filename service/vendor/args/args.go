3b353365dca0033579ce1b42146f2022e4effc3d
VinaLx <vinaleux@gmail.com>
2017-12-08 23:55:06 +0800

    initialize service 2

--- service/vendor/args/args.go
+++ service/vendor/args/args.go
@@ -1,1 +1,21 @@
 package args
+
+import (
+	"github.com/ogier/pflag"
+)
+
+// Host ..
+var Host *string
+
+// Port ..
+var Port *uint16
+
+// DB is the path to the sqlite db file
+var DB *string
+
+func init() {
+	Host = pflag.StringP("host", "h", "0.0.0.0", "Host of the server listening on")
+	Port = pflag.Uint16P("port", "p", 8080, "Port of the server listening on")
+	DB = pflag.StringP("db", "d", "agenda.db", "Path to the sqlite db file")
+	pflag.Parse()
+}
--- service/vendor/database/database.go
+++ service/vendor/database/database.go
@@ -1,1 +1,61 @@
 package database
+
+import (
+	"args"
+	"database/sql"
+	"entity"
+	"log"
+)
+
+const createTableSQL = `CREATE TABLE IF NOT EXISTS User (
+	id INTEGER NOT NULL PRIMARY KEY,
+	username TEXT NOT NULL,
+	password TEXT NOT NULL,
+	phone TEXT NOT NULL,
+	email TEXT NOT NULL
+);`
+
+type agendaDB struct {
+	db *sql.DB
+}
+
+var theDB *agendaDB
+
+func prepareDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", *args.DB)
+	// open database fail
+	if err != nil {
+		log.Printf("ERROR: %s", err.Error())
+		return nil, err
+	}
+	_, err = db.Exec(createTableSQL)
+	// create table fail
+	if err != nil {
+		return nil, err
+	}
+	log.Print("database initialized")
+	return db, nil
+}
+
+func getUserDAO() (entity.UserDAO, error) {
+	if theDB == nil {
+		db, err := prepareDB()
+		if err != nil {
+			return nil, err
+		}
+		theDB = &agendaDB{db}
+	}
+	return theDB, nil
+}
+
+func (d *agendaDB) GetAllUsers() []*entity.User {
+	return nil
+}
+
+func (d *agendaDB) StoreUsers(user *entity.User) {
+
+}
+
+func (d *agendaDB) GetUser(username string) *entity.User {
+	return nil
+}
