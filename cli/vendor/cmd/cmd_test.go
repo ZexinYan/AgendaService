f84c7c683eac93603a1acc51ced9dd4f63013aa8
Zexin Yan <yzx9610@outlook.com>
2017-12-12 22:58:13 +0800

    update cmd test code

--- cli/vendor/cmd/cmd_test.go
+++ cli/vendor/cmd/cmd_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRegister(t *testing.T) {
 	fmt.Println("Testing Register...")
-	got := Register("yanzexin", "123", "mail", "1234")
+	got := Register("yanzexin", "123", "mail", "1234", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
@@ -17,7 +17,7 @@ func TestRegister(t *testing.T) {
 
 func TestLogin(t *testing.T) {
 	fmt.Println("Testing Login...")
-	got := Login("yanzexin", "123")
+	got := Login("yanzexin", "123", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
@@ -27,8 +27,8 @@ func TestLogin(t *testing.T) {
 
 func TestLogout(t *testing.T) {
 	fmt.Println("Testing Log out...")
-	Login("yanzexin", "123")
-	got := Logout()
+	Login("yanzexin", "123", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
+	got := Logout("https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
@@ -38,8 +38,8 @@ func TestLogout(t *testing.T) {
 
 func TestShowInfo(t *testing.T) {
 	fmt.Println("Testing show information...")
-	Login("yanzexin", "123")
-	got := ShowInfo("yanzexin")
+	Login("yanzexin", "123", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
+	got := ShowInfo("yanzexin", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
@@ -49,8 +49,8 @@ func TestShowInfo(t *testing.T) {
 
 func TestShowUsers(t *testing.T) {
 	fmt.Println("Testing show users...")
-	Login("yanzexin", "123")
-	got := ShowUsers()
+	Login("yanzexin", "123", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
+	got := ShowUsers("https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
@@ -60,8 +60,8 @@ func TestShowUsers(t *testing.T) {
 
 func TestDeleteUser(t *testing.T) {
 	fmt.Println("Testing delete user...")
-	Login("yanzexin", "123")
-	got := DeleteUser("yanzexin")
+	Login("yanzexin", "123", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
+	got := DeleteUser("yanzexin", "https://private-anon-f5f6a74e5f-agendaservice2.apiary-mock.com")
 	expect := 0
 
 	if got != expect {
